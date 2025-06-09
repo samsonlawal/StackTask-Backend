df08ca58f9859f5a3358a2f10ac30887e0b9aa41
deji <lawal4211@gmail.com>
2025-06-09 02:31:59 +0100

    priority enum correcction

--- models/task.model.js
+++ models/task.model.js
@@ -12,8 +12,8 @@ const taskSchema = new mongoose.Schema(
     },
     priority: {
       type: String,
-      enum: ["low", "medium", "high"],
-      default: "low",
+      enum: ["Low", "Medium", "High"],
+      default: "Low",
     },
     assignee: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     workspace_id: {
