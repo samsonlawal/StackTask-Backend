0ad6eb3908de34c9f54e8fbad135a521dea9bf39
deji <lawal4211@gmail.com>
2026-08-06 21:01:46 +0100

    fix: password double hashing

--- models/user.model.js
+++ models/user.model.js
@@ -50,6 +50,7 @@ const userSchema = new mongoose.Schema(
 
 // Hash password
 userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt();
   this.password = await bcrypt.hash(this.password, salt);
   next();
